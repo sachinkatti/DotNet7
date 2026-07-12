52932fb70b6d0abcd754e13bd6127e5fcb7d4d5a
sachinkatti <sachinkatti19@gmail.com>
2026-07-12 22:53:53 +0530

    default constructor example

---
+++ DefaultConstructorExample/DefaultConstructorExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultConstructorExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Company company = new Company("TCS");
+            company.CompanyDirector();
+            
+            Company company1 = new Company();
+            Console.ReadLine();
+        }
+    }
+}
