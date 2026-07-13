d7164fdbef39796785f5d94e3b33f05031a7cd4a
sachinkatti <sachinkatti19@gmail.com>
2026-07-13 21:30:18 +0530

    constructor example

---
+++ CtorExample/CtorExample/Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtorExample
+{
+    public class Model
+    {
+        public Model(int id)
+        {
+            Console.WriteLine("A");
+        }
+
+        public Model(string name, int id)
+        {
+            Console.WriteLine("B");
+        }
+    }
+}
