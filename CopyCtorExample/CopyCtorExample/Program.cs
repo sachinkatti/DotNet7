b052cdc05cc85d928f30701caee1c15426d86520
sachinkatti <sachinkatti19@gmail.com>
2026-07-13 21:31:32 +0530

    constructor example

---
+++ CopyCtorExample/CopyCtorExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyCtorExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address tempAddress = new Address("Baner", "Pune");
+            Address perAddress = new Address(tempAddress);
+
+            perAddress.PrintAddress();
+        }
+    }
+}
