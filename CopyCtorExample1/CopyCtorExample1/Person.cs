d18cc4cbe516af71584b50137ddabed9ff786d11
sachinkatti <sachinkatti19@gmail.com>
2026-07-13 21:32:43 +0530

    constructor example

---
+++ CopyCtorExample1/CopyCtorExample1/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyCtorExample1
+{
+    public class Person
+    {
+        public int age;
+        public string name;
+    
+
+    public Person(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        public Person (Person person)
+        {
+            age = person.age;
+            name = person.name;
+        }
+
+        public void PrintPerson()
+        {
+            Console.WriteLine("The person age is :"+ age + " "+ "Name called :"+name);
+        }
+
+        public void DisplayPerson()
+        {
+            PrintPerson();
+        }
+    }
+}
