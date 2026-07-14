38094bd76b0fa9c2a17137501fdc31c41360ba25
sachinkatti <sachinkatti19@gmail.com>
2026-07-14 11:43:18 +0530

    static class examples

---
+++ MathStaticExample/MathStaticExample/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathStaticExample
+{
+    public static class Calculator
+    {
+        public static int  add(int a, int b)
+        {
+            return a+b;
+        }
+
+        public static int substract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int multi(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double divide(int a, int b)
+        {
+            return (double) a / b;
+        }
+    }
+}
