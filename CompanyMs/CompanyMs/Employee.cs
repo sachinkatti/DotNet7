24cf975fa7be3128ac77082b07c1d0b6b0e8c63f
sachinkatti <sachinkatti19@gmail.com>
2026-07-15 23:23:32 +0530

    example of companyms

---
+++ CompanyMs/CompanyMs/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyMs
+{
+    public class Employee
+    {
+        public decimal GetId()
+        {
+            return 2.8m;
+        }
+    }
+}
