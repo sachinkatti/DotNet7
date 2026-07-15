d91b13841ed3e98af818295530ecd969cd00db1f
sachinkatti <sachinkatti19@gmail.com>
2026-07-15 23:16:08 +0530

    exampale of company how to get data

---
+++ ExampleCompany/ExampleCompany/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompanyMs;
+
+namespace ExampleCompany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            Console.WriteLine( emp.GetId());
+
+        }
+    }
+}
