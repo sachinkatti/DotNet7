e5b345a4593b055e24c78a3936d8db7d9f4e3df3
sachinkatti <sachinkatti19@gmail.com>
2026-07-15 23:24:48 +0530

    account holder handsOn

---
+++ AccountHolder/AccountHolder/MyPin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountHolder
+{
+    public class MyPin
+    {
+        private MyPin() 
+        {
+
+        }
+
+        public MyPin(string pin)
+        {
+            Console.WriteLine(" My pin is :" +pin);
+        }
+
+        public void ShowPin()
+        {
+            Console.WriteLine(" My pin is 1234 ");
+        }
+    }
+}
