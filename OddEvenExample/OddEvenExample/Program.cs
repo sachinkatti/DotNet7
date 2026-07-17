c04a282b1c188b12c342c34d0996bde2eb65a392
sachinkatti <sachinkatti19@gmail.com>
2026-07-17 23:08:23 +0530

    odd and even example

---
+++ OddEvenExample/OddEvenExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEvenExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+         {
+            int number;
+
+            Console.WriteLine("Enter a number");
+            number = Convert.ToInt32(Console.ReadLine());
+
+           if(number % 2 == 0)
+            {
+                Console.WriteLine("Its a even");
+            }
+            else
+            {
+                Console.WriteLine("its a odd");
+
+            }
+        }
+    }
+}
