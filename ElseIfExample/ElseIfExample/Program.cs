13d98254cdb214ecf355eef7dcce7addd96b1f7e
sachinkatti <sachinkatti19@gmail.com>
2026-07-17 23:09:23 +0530

    else if statements example

---
+++ ElseIfExample/ElseIfExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElseIfExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int marks;
+
+            Console.WriteLine("Enter Your Marks");
+            marks = Convert.ToInt32(Console.ReadLine());
+
+            if (marks > 90)
+            {
+                Console.WriteLine("Distinction");
+            }
+            else if (marks > 80)
+            {
+                Console.WriteLine("First Class");
+            }
+            else if (marks > 70)
+            {
+                Console.WriteLine("Second Class");
+            }
+            else if (marks > 35)  
+            {
+                Console.WriteLine("All Exam Pass");
+            }
+            else
+            {
+                Console.WriteLine("Fail"); 
+            }
+        }
+    }
+}
