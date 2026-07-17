c453eed5d6adb3b50251c3b681f5b45138ddbfbe
sachinkatti <sachinkatti19@gmail.com>
2026-07-17 23:07:09 +0530

    if else statements example

---
+++ IfExample/IfExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age > 18)
+            {
+                Console.WriteLine("You are an adult");
+            }
+            else
+            {
+                Console.WriteLine("You are minor");
+            }
+        }
+    }
+}
